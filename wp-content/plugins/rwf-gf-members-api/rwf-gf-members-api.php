9479bd27c47c7c8e8145f9b6900308ef05b62774
jamesgreenblue <j@greenhalgh.io>
2020-08-16 17:45:24 +0800

    Top 5 members shortcode finalised, added email alerts for fatal API errors

--- wp-content/plugins/rwf-gf-members-api/rwf-gf-members-api.php
+++ wp-content/plugins/rwf-gf-members-api/rwf-gf-members-api.php
@@ -1,21 +1,22 @@
 <?php
 /*
-Plugin Name: Get Green Fins member info from API
-description: Add Green Fins member information to pages and posts using shortcodes (cached using WP transients)
+Plugin Name: Display Green Fins member info from Portal API
+Plugin URI: https://reef-world.org
+Description: Display Green Fins member information within pages and posts using shortcodes (cached using WP transients) from the Portal API
 Version: 1.0
 Author: James Greenhalgh
+Author URI: https://www.instagram.com/jamesgreenblue/
 License: GPLv2 or later
 Text Domain: rwf-gf-members-api
 */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined( 'ABSPATH' ) or die( 'No script kiddies please' );
 
 /**
  * List Top 10 Members
  *
  * Outputs a list of the top 10 members from the Portal API, cached for 4 hours using WP transients
  */
-
 function list_top10members_func( $atts ) {
 
    $top10members = get_transient( 'get_top10members' );
@@ -35,7 +36,8 @@ function list_top10members_func( $atts ) {
    // Handle the case when there are no members or the API is malfunctioning
    if ( empty($top10members) || $top10members["success"] == 0) {
       delete_transient( 'get_top10members' );
-      return "<strong>" . __("No members to show, unable to fetch assessment data from API", 'rwf-gf-members-api') . "</strong>";
+      wp_mail("it@reef-world.org", "Alert: Error triggered on Green Fins Wordpress site", "Error: Unable to fetch data from Portal API. Please investigate.");
+      return "<strong>" . __("Error: Unable to fetch data from Portal API. Please notify it@reef-world.org", 'rwf-gf-members-api') . "</strong>";
    }
 
    // We're going to return a string. First, we open a list.
@@ -70,48 +72,81 @@ LISTING;
 }
 
 
+
+
+
+
+
+
+
+
 /**
  * List Top 5 Members by country
  *
  * Outputs a list of the top 5 members for a given country from the Portal API, cached for 4 hours using WP transients
  */
-
 function list_top5bycountry_func( $atts = [] ) {
-   // normalize attribute keys, lowercase
-   $atts = array_change_key_case( (array) $atts, CASE_LOWER );
-
    // override default attributes with user attributes
    $get_atts = shortcode_atts(
       array(
-         'country' => 'Please specify a country to display the top 5 list',
+         'country' => 'Please specify a valid country to display the top 5 list',
       ), $atts
    );
 
-   return $get_atts['country'];
-
-
-/**
-   $top5bycountry = get_transient( 'get_top5bycountry' );
-   if ( false === $top5bycountry ) {
+   //load the countries list
+   $countries = get_transient( 'get_countries' );
+   if ( false === $countries ) {
        // Transient expired, refresh the data
 
-      // resolve the country name to the ID
-       $countryid =
-
        $key = 'iQZ35gareSKwo9YfHVJyvpXPhE8LA7';
-       $url = 'https://portal-dev.greenfins.net/api/v1/countries/' . $countryid . '/top-10-members??key=' . $key;
+       $url = 'https://portal-dev.greenfins.net/api/v1/countries?key=' . $key;
        $data = wp_remote_get($url);
        $response = wp_remote_retrieve_body($data);
        $json = json_decode($response, true);
 
-       set_transient( 'get_top5bycountry', $json, 4 * HOUR_IN_SECONDS );
-       $top5bycountry = get_transient( 'get_top5bycountry' );
+       set_transient( 'get_countries', $json, 4 * HOUR_IN_SECONDS );
+       $countries = get_transient( 'get_countries' );
+   }
+   // Handle the case when there are no countries or the API is malfunctioning
+   if ( empty($countries) || $countries["success"] == 0) {
+      delete_transient( 'get_countries' );
+      wp_mail("it@reef-world.org", "Alert: Error triggered on Green Fins Wordpress site", "Error: Unable to fetch data from Portal API. Please investigate.");
+      return "<strong>" . __("Error: Unable to fetch data from Portal API. Please notify it@reef-world.org", 'rwf-gf-members-api') . "</strong>";
+   }
+
+   // lookup the country id by the user specified text
+   $countries = $countries['data'];
+   $userinput = $get_atts['country'];
+   $result = array_column($countries,"id","name");
+   if(isset($result[$userinput])){
+      $countryid = $result[$userinput];
+   }
+
+   // Handle the case where an invalid option is supplied
+   if(!isset($countryid)) {
+      return "<strong>" . __("Error: Invalid country specified, please check your spelling.", 'rwf-gf-members-api') . "</strong>";
    }
    
+   $transient_name = 'get_top5bycountry_' . $userinput;
+
+   $top5bycountry = get_transient( $transient_name );
+   if ( false === $top5bycountry ) {
+       // Transient expired, refresh the data
+
+       $key = 'iQZ35gareSKwo9YfHVJyvpXPhE8LA7';
+       $url = 'https://portal-dev.greenfins.net/api/v1/countries/' . $countryid . '/top-5-members?key=' . $key;
+       $data = wp_remote_get($url);
+       $response = wp_remote_retrieve_body($data);
+       $json = json_decode($response, true);
+
+       set_transient( $transient_name, $json, 4 * HOUR_IN_SECONDS );
+       $top5bycountry = get_transient( $transient_name );
+   }
    // Handle the case when there are no members or the API is malfunctioning
    if ( empty($top5bycountry) || $top5bycountry["success"] == 0) {
-      delete_transient( 'get_top5bycountry' );
-      return "<strong>" . __("No members to show, unable to fetch assessment data from API", 'rwf-gf-members-api') . "</strong>";
+      delete_transient( $transient_name );
+      wp_mail("it@reef-world.org", "Alert: Error triggered on Green Fins Wordpress site", "Error: Unable to fetch data from Portal API. Please investigate.");
+      return "<strong>" . __("Error: Unable to fetch data from Portal API. Please notify it@reef-world.org", 'rwf-gf-members-api') . "</strong>";
    }
 
    // We're going to return a string. First, we open a list.
@@ -143,7 +178,7 @@ LISTING;
    $return .= "</ul>";
 
    return $return;
-   */
+
 }
 
 /**
