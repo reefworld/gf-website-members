21cb868189a345e5a03757bdd4fd7bfaa282b47f
James Greenhalgh <j@greenhalgh.io>
2023-04-15 10:17:45 +0800

    Fix: Remove levels from inactive listings
    Needed the restricted level only to filter these results out from the main pages but still have them available to the verify tool.

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://reef-world.org/donate
 Tags: api, reef-world, greenfins
 Requires at least: 6.2
 Tested up to: 6.2
-Stable tag: 2023.1
+Stable tag: 2023.2
 Requires PHP: 8.0
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
--- rwf-gf-website-members.php
+++ rwf-gf-website-members.php
@@ -3,7 +3,7 @@
 Plugin Name: Active, Inactive & Restricted Green Fins member listing and verification (Hub API)
 Plugin URI: https://reef-world.org
 Description: Display Green Fins member info on maps, pages and posts sourced from Green Fins Hub API. Requires WP Store Locator v2.2.236 or later.
-Version: 2023.1
+Version: 2023.2
 Author: James Greenhalgh
 Author URI: https://jamesgreenblue.com
 License: GPLv3
@@ -702,15 +702,6 @@ function rwf_gf_fetch_members_as_posts_func()
          } else {
             $term_id = array(); //unset category for inactive memebrs
             switch ($member['membership_level']) {
-               case "Certified Bronze Member":
-                  $member['membership_level'] = "3";
-                   break;
-               case "Certified Silver Member":
-                  $member['membership_level'] = "2";
-                   break;
-               case "Certified Gold Member":
-                  $member['membership_level'] = "1";
-                   break;
                case "Restricted":
                   $member['membership_level'] = "restricted";
                    break;
